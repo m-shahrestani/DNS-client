20b318014bd3e5cae3d1a6e1fa23bd68492e9029
m-shahrestani <9728054@ceit.local>
2021-04-13 18:05:51 +0430

    Add Part2

---
+++ localServer.py
@@ -0,0 +1,22 @@
+# part 1-3
+
+import socket
+
+local_address = ("127.0.0.1", 20001)
+bufferSize = 1024
+
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPServerSocket.bind(local_address)
+print("UDP server up and listening")
+
+while True:
+    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    message = bytesAddressPair[0]
+    address = bytesAddressPair[1]
+    clientMsg = "Message from Client: " + message.decode()
+    clientIP = "Client IP Address:{}".format(address)
+    msgFromServer = "Hello UDP Client your massage is: " + message.decode()
+    bytesToSend = str.encode(msgFromServer)
+    print(clientMsg)
+    print(clientIP)
+    UDPServerSocket.sendto(bytesToSend, address)
